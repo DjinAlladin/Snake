0db19ae32fcd4f77bc786ac52851efcb84bdd283
DijnAlladin <ahha1966@tut.by>
2021-01-03 18:13:53 +0300

    Hello Word

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Привет Мир");
+            Console.ReadKey();
+        }
+    }
+}
